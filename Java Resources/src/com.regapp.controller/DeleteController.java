e712fa0ea6c0c77231a981765488c21912b292bd
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-04-19 10:13:57 +0530

    Update DeleteController.java

--- Java Resources/src/com.regapp.controller/DeleteController.java
+++ Java Resources/src/com.regapp.controller/DeleteController.java
@@ -1,1 +1,48 @@
+package com.regapp.controller;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.regapp.model.DAOService;
+import com.regapp.model.DAOServiceImpl;
+
+@WebServlet("/delete")
+public class DeleteController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+    public DeleteController() {
+    	
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String email = request.getParameter("email");
+		// we take the email which has to delete
+		
+		DAOService service = new DAOServiceImpl();
+		service.connectDB();  // call for connect the DB
+		
+		service.deleteByEmail(email);  // call for delete the record
+		
+		// after delete record we want to see remaining records on same webpage
+		
+		ResultSet result = service.getAllReg();
+		// call for to get all registrations details
+		
+		request.setAttribute("result", result);
+		RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/list_registration.jsp");
+		// it will redirect to the same page after delete the record
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+}
